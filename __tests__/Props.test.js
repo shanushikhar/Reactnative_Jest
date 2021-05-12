35caf746c299b7f350f5bc745dbeaf0e36529540
shanu shikhar <thisisshanushikhar@gmail.com>
2021-05-12 19:53:00 +0530

    adding button click event with state modification

--- __tests__/Props.test.js
+++ __tests__/Props.test.js
@@ -9,6 +9,29 @@ configure({ adapter: new Adapter() })
 
 test('Testing props', () => {
     const wrapper = shallow(<Home data='props data' />).props()
-    console.log(wrapper.children[1].props.children[1])
+    // console.log(wrapper.children[1].props.children[1])
     expect(wrapper.children[1].props.children[1]).toEqual('props data')
+})
+
+describe('App test', () => {
+
+    // console.log(wrapper.state().data)
+    test('Testing state', () => {
+        let wrapper = shallow(<Home />)
+        expect(wrapper.state().data).toEqual('data added...')
+    })
+
+    test('testing button', () => {
+
+        let wrapper = shallow(<Home />)
+
+        expect(wrapper.find('#refreshButton')).toHaveLength(1);
+
+        let btn = wrapper.find('#refreshButton')
+        btn.props().onPress()
+
+        expect(wrapper.state().count).toEqual(1)
+
+    })
+
 })
--- src/Home.js
+++ src/Home.js
@@ -1,20 +1,33 @@
 import React, { Component } from 'react'
-import { Text, TextInput, View } from 'react-native'
+import { Button, Text, TextInput, View } from 'react-native'
 import Users from './users'
 
 export default class Home extends Component {
     state = {
-        data: ''
+        data: '',
+        count: 0
     }
 
     componentDidMount() {
         Users.all().then(data => { })
+        this.setState({
+            data: 'data added...'
+        })
     }
 
     change(x) {
         //  return x * 5  
 
         this.setState({ data: x * 5 })
+    }
+
+    refreshData = () => {
+        console.log("download data from server placeholder");
+        this.setState(
+            (prevState, props) => {
+                return { count: prevState.count + 1 };
+            }
+        );
     }
 
     render() {
@@ -34,6 +47,14 @@ export default class Home extends Component {
                     style={{ backgroundColor: 'grey', marginBottom: 15 }}
                     placeholder='Enter password'
                     placeholderTextColor='red'
+                />
+                <Button
+                    id="refreshButton"
+                    onPress={this.refreshData}
+                    title="Refresh"
+                    color="#000000"
+                    accesibilityLabel="Refresh AQI data"
+                    testID="refreshButton"
                 />
             </View>
         )
